3da7a74c920fe1f3a8dc2b3799a9c25b6726114b
SparkingWater <dustin.meyer.001@gmail.com>
2026-03-05 09:43:43 -0800

    Create Day_1.py
    Day 1 challange

---
+++ Day_1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city_name = input("whats's the name of the City your grew up in?\n")
+pet_name = input("what's your pet's name?\n")
+band_name =city_name + " " + pet_name
+print("your band name is" + " " + band_name)
